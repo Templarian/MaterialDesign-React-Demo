0b3c0d323ee99f0430b91c9f9e202236accd31ea
Austin Andrews <admin@templarian.com>
2022-04-16 13:10:37 -0500

    Bump React v18.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import Icon, { Stack } from '@mdi/react';
 import {
   mdiAccount, mdiAccountOutline, mdiAccountSearch, mdiLoading,
@@ -221,4 +221,6 @@ const App = (props) => (
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
